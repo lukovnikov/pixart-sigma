114fdfb8e0e375047485de2b0e07d16c79a80a82
Denis lukovnikov <lukovnikov.denis@gmail.com>
2024-07-19 21:08:44 +0200

    safe commit

--- cocodata.py
+++ cocodata.py
@@ -378,29 +378,6 @@ class COCOPanopticDataset(Dataset):
         tokenizer = tokenizer if tokenizer is not None else self.tokenizer
         ret = tokenizer.decode(x)
         return ret
-    
-    # def __iter__(self):
-    #     self.n = 0
-    #     if self.shuffle:
-    #         for k, v in self.examples:
-    #             random.shuffle(v)
-    #     return self
-    
-    # def __next__(self):
-    #     if self.n >= self.total_n:
-    #         raise StopIteration()
-        
-    #     prevc, c = 0, 0
-    #     # find bucket
-    #     for k, v in self.examples:
-    #         prevc = c
-    #         c += len(v)
-    #         if prevc <= self.n < c:
-    #             break
-    #     # get example
-    #     example = v[self.n - prevc]
-    #     # increase count
-    #     self.n += 1
         
     def __getitem__(self, item):
         example = self.examples[item]
@@ -481,6 +458,7 @@ class COCOPanopticDataset(Dataset):
                 "regionmasks": batched_regionmasks, 
                 "captiontypes": captiontypes}
         
+        
     def materialize_example(self, example):
         def region_code_to_rgb(rcode):
             B = rcode // 256**2
@@ -505,116 +483,24 @@ class COCOPanopticDataset(Dataset):
         seg_imgtensor = torch.tensor(np.array(seg_img)).permute(2, 0, 1)
         
         # 3. create conditioning image by randomly swapping out colors
-        cond_imgtensor = torch.ones_like(imgtensor) * torch.tensor(randomcolor_hsv())[:, None, None]
+        # cond_imgtensor = torch.ones_like(imgtensor) * torch.tensor(randomcolor_hsv())[:, None, None]
+        cond_imgtensor = torch.zeros(self.max_masks + 1, imgtensor.size(1), imgtensor.size(2), dtype=torch.long, device=imgtensor.device)
         
         # 4. pick one caption at random (TODO: or generate one from regions)
         captions = [random.choice(example.captions)]
     
         # 4. load masks
-        # masks = [torch.ones_like(imgtensor[0], dtype=torch.bool)]
-        # get the captions of the regions and build layer ids
-        # region_code_to_layerid = {0: 0}
-        # region_code_to_layerid = {}
-        
-        # region_caption_to_layerid = {}
-        # unique_region_captions = set()
-        
-        # for _, region_info in example.seg_info.items():
-        #     unique_region_captions.add(region_info["caption"])
+        ids = list(range(1, self.max_masks+1))
+        random.shuffle(ids)
         
         for i, (region_code, region_info) in enumerate(example.seg_info.items()):
             rgb = torch.tensor(region_code_to_rgb(region_code))
             region_mask = (seg_imgtensor == rgb[:, None, None]).all(0)
             if (region_mask > 0).sum() / np.prod(region_mask.shape) < self.min_region_area:
                 continue
-            # if self.casmode is None or self.casmode.name != "global":
-            #     region_caption = region_info["caption"]
-            #     if region_caption in unique_region_captions:
-            #         if (not self.mergeregions) or (region_caption not in region_caption_to_layerid):
-            #             new_layerid = len(masks)
-            #             region_caption_to_layerid[region_caption] = new_layerid
-            #             captions.append(region_info["caption"])
-            #             masks.append(region_mask)
-            #         else:
-            #             new_layerid = region_caption_to_layerid[region_caption]
-            #             masks[new_layerid] = masks[new_layerid] | region_mask        
-            #         region_code_to_layerid[region_code] = new_layerid            
-            #     else:
-            #         pass #continue    # or pass? (if pass, the mask will end up in the conditioning image for controlnet)
+            cond_imgtensor[ids[i]] = region_mask
             
-            randomcolor = torch.tensor(randomcolor_hsv()) #if self.casmode is not None else torch.tensor(randomcolor_predef())
-            maskcolor = region_mask.unsqueeze(0).repeat(3, 1, 1) * randomcolor[:, None, None]
-        
-            cond_imgtensor = torch.where(region_mask.unsqueeze(0) > 0.5, maskcolor, cond_imgtensor)
-            
-            
-        # append extra global prompt
-        # extraexpressions = ["This image contains", "In this image are", "In this picture are", "This picture contains"]
-        # if (self.casmode.name == "doublecross") and not ("keepprompt" in self.casmode.chunks or "test" in self.casmode.chunks):
-        #     # assert not self.simpleencode
-        #     captions[0] += ". " + random.choice(extraexpressions) + " " + ", ".join([e for e in captions[1:]]) + "."
-            
-            
-        # minimize_length = False
-        
-        # if self.casmode.use_global_prompt_only:   # self.casmode in ("posattn-opt", "posattn2-opt"):
-        #     caption = captions[0] if not self.casmode.augment_only else ""
-        #     if self.casmode.augment_global_caption:       # if training, automatically augment sentences
-        #         tojoin = []
-        #         for i, capt in enumerate(captions[1:]):
-        #             tojoin.append(f"{{{capt}:{i}}}")
-        #         caption += " " + random.choice(extraexpressions) + " " + ", ".join(tojoin) + "."
-        #     _caption, _layerids = _tokenize_annotated_prompt(caption, tokenizer=self.tokenizer, minimize_length=minimize_length)
-        #     # replace region codes with layer ids
-        #     for region_code, layerid in region_code_to_layerid.items():
-        #         _layerids = torch.masked_fill(_layerids, _layerids == region_code + 1, layerid)
-        #     captions, layerids = [_caption], [_layerids]
-        #     encoder_layerids = [torch.ones_like(layerids[-1]) * 0]
-        # # elif self.simpleencode: #  or (self.casmode in ("posattn-opt", "posattn2-opt") and not ("keepprompt" in self.casmodechunks or "test" in self.casmodechunks)):   # or self.casmode == "doublecross":
-        # #     tojoin = []
-        # #     for i, capt in enumerate(captions[1:]):
-        # #         tojoin.append(f"{{{capt}:{i}}}")
-        # #     captions[0] += ". " + random.choice(extraexpressions) + " " + ", ".join(tojoin) + "."
-            
-        # #     _captions, _layerids = _tokenize_annotated_prompt(captions[0], tokenizer=self.tokenizer, minimize_length=minimize_length)
-        # #     captions, layerids = [_captions], [_layerids]
-        # #     encoder_layerids = [torch.ones_like(layerids[-1]) * 0]
-        # else:
-        #     # encode separately
-        #     tokenized_captions = []
-        #     layerids = []
-        #     encoder_layerids = []
-        #      #self.casmode != "global"
-             
-        #     if self.casmode.augment_global_caption:
-        #         caption = captions[0] if not self.casmode.augment_only else ""
-        #         caption += ". " + random.choice(extraexpressions) + " " + ", ".join([e for e in captions[1:]]) + "."
-        #         captions[0] = caption
-             
-        #     for i, caption in enumerate(captions):
-        #         if i == 0:
-        #             tokenized_caption, layerids_e = _tokenize_annotated_prompt(caption, tokenizer=self.tokenizer, minimize_length=minimize_length)
-        #             # replace region codes with layer ids
-        #             for region_code, layerid in region_code_to_layerid.items():
-        #                 layerids_e = torch.masked_fill(layerids_e, layerids_e == region_code + 1, layerid)
-        #             tokenized_captions.append(tokenized_caption)
-        #             layerids.append(layerids_e)    
-        #         else:
-        #             tokenized_captions.append(self.tokenize(caption, tokenizer=self.tokenizer, minimize_length=minimize_length)[0])
-        #             layerids.append(torch.ones_like(tokenized_captions[-1]) * i)    
-        #         encoder_layerids.append(torch.ones_like(layerids[-1]) * i)
-        #     captions = tokenized_captions
-        #     if self.casmode.replace_layerids_with_encoder_layerids:
-        #         layerids[0] = encoder_layerids[0]
-                
-        # if self.limitpadding:
-        #     for i in range(len(captions)):
-        #         caption = captions[i]
-        #         numberpads = (caption == self.tokenizer.pad_token_id).sum()     # assumption: all pads are contiguous and at the end
-        #         endidx = -(numberpads - self.padlimit)
-        #         captions[i] = caption[:endidx]
-        #         layerids[i] = layerids[i][:endidx]
-        #         encoder_layerids[i] = encoder_layerids[i][:endidx]
+        cond_imgtensor[0] = cond_imgtensor[1:].long().sum(0) == 0
 
         # random square crop of size divisble by 64 and maximum size 512
         cropsize = min((min(imgtensor[0].shape) // 64) * 64, 512)
@@ -624,40 +510,6 @@ class COCOPanopticDataset(Dataset):
         
         imgtensor = imgtensor[:, crop[0]:crop[0]+cropsize, crop[1]:crop[1]+cropsize]
         cond_imgtensor = cond_imgtensor[:, crop[0]:crop[0]+cropsize, crop[1]:crop[1]+cropsize]
-        # masks = [maske[crop[0]:crop[0]+cropsize, crop[1]:crop[1]+cropsize] for maske in masks]
-        
-        # # compute downsampled versions of the layer masks
-        # downsamples = [cropsize // e for e in [8, 16, 32, 64]]
-        # downmaskses = []
-        # for mask in masks:
-        #     downmasks = {}
-        #     for downsample in downsamples:
-        #         downsampled = _img_importance_flatten(mask.float(), downsample, downsample)
-        #         downmasks[tuple(downsampled.shape)] = downsampled
-        #     downmaskses.append(downmasks)
-            
-        # # concatenate masks in one tensor
-        # downmasktensors = {}
-        # for downmasks in downmaskses:
-        #     for res, downmask in downmasks.items():
-        #         if res not in downmasktensors:
-        #             downmasktensors[res] = []
-        #         downmasktensors[res].append(downmask)
-        # downmasktensors = {k: torch.stack(v, 0) for k, v in downmasktensors.items()}
-        
-        # # if usescribbles, transform cond_imgtensor to scribbles
-        # if self.usescribbles:
-        #     cond_imgtensor_np = (np.array(cond_imgtensor.permute(1, 2, 0)) * 255).astype("uint8")
-        #     canny = cv.Canny(cond_imgtensor_np, 50, 100)
-        #     scribbleradius = 5      # TODO: check scribble brush size
-        #     brush = np.zeros((scribbleradius*2+1, scribbleradius*2+1))
-        #     cv.circle(brush, (scribbleradius, scribbleradius), 0, 1, scribbleradius*2)
-        #     cond_scribbles = cv.filter2D(src=canny, ddepth=-1, kernel=brush) > 0
-        #     cond_imgtensor = torch.tensor(cond_scribbles)[None].repeat(3, 1, 1).to(cond_imgtensor.dtype).to(cond_imgtensor.device)
-            
-        # if self.usecanny:
-        #     canny = example.canny_data
-        #     cond_imgtensor = to_tensor(canny).repeat(3, 1, 1)
             
         imgtensor = imgtensor * 2 - 1.
         
@@ -670,37 +522,6 @@ class COCOPanopticDataset(Dataset):
                 }
     
     
-class COCODataLoader(object):
-    def __init__(self, cocodataset, batch_size=2, shuffle=False, num_workers=0, repeatexample=False) -> None:
-        super().__init__()
-        self.ds = cocodataset
-        self.dataloaders = []
-        self.batch_size = min(batch_size.values()) if isinstance(batch_size, dict) else batch_size
-        self.repeatexample = repeatexample
-            
-        for k, cocosubset in self.ds.examples:
-            if isinstance(batch_size, dict):
-                batsize = batch_size[k]
-            else:
-                batsize = batch_size
-                
-            collatefn = cocodataset.collate_fn
-            if self.repeatexample:
-                collatefn = lambda examples: cocodataset.collate_fn(examples * batch_size)
-                batsize = 1
-                
-            subdl = DataLoader(COCODatasetSubset(cocosubset, self.ds), 
-                                       batch_size=batsize, 
-                                       collate_fn=collatefn, 
-                                       shuffle=shuffle,
-                                       num_workers=num_workers)
-            self.dataloaders.append(subdl)
-        self.subdls_lens = [len(dl) for dl in self.dataloaders]
-        # print(self.subdls_lens)
-        
-    def __iter__(self):
-        return itertools.chain(*[iter(subdl) for subdl in self.dataloaders])
-        
 
 # !!! IDEA: only use the higher-res images for training later denoising steps
     
--- train_control_ae.py
+++ train_control_ae.py
@@ -1,8 +1,14 @@
+import colorsys
+from pathlib import Path
+import random
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+import tqdm
 
 from cocodata import COCOPanopticDataset, COCOPanopticExample
+from torchvision import transforms
+from PIL import Image
 
 import pytorch_lightning as pl
 
@@ -24,26 +30,28 @@ class ControlSignalEncoder(torch.nn.Module):
         self.inchannels = inchannels
         self.outchannels = outchannels
         self.layers = torch.nn.Sequential(
-            torch.nn.Conv2d(inchannels, 16, 3, padding=1),
-            torch.nn.SiLU(),
-            torch.nn.Conv2d(16, 16, 3, padding=1),
-            torch.nn.SiLU(),
-            torch.nn.Conv2d(16, 32, 3, padding=1, stride=2),
+            torch.nn.Conv2d(inchannels, 32, 3, padding=1),
             torch.nn.SiLU(),
             torch.nn.Conv2d(32, 32, 3, padding=1),
             torch.nn.SiLU(),
-            torch.nn.Conv2d(32, 96, 3, padding=1, stride=2),
+            torch.nn.Conv2d(32, 64, 3, padding=1, stride=2),
             torch.nn.SiLU(),
-            torch.nn.Conv2d(96, 96, 3, padding=1),
+            torch.nn.Conv2d(64, 64, 3, padding=1),
             torch.nn.SiLU(),
-            torch.nn.Conv2d(96, 256, 3, padding=1, stride=2),
+            torch.nn.Conv2d(64, 128, 3, padding=1, stride=2),
             torch.nn.SiLU(),
-            torch.nn.Conv2d(256, 256, 3, padding=1),
+            torch.nn.Conv2d(128, 128, 3, padding=1),
             torch.nn.SiLU(),
-            torch.nn.Conv2d(256, outchannels*2, 1, padding=0)
+            torch.nn.Conv2d(128, 256, 3, padding=1, stride=2),
+            # torch.nn.SiLU(),
+            # torch.nn.Conv2d(128, 128, 3, padding=1),
+            torch.nn.SiLU(),
+            torch.nn.Conv2d(256, outchannels*2, 1, padding=0),
+            # torch.nn.Tanh(),
         )
         
     def forward(self, x):
+        x = x.float()
         for layer in self.layers:
             x = layer(x)
         mu, logvar = x[:, :self.outchannels], x[:, self.outchannels:]
@@ -62,17 +70,21 @@ class ControlSignalDecoder(torch.nn.Module):
             nn.ConvTranspose2d(inchannels, 128, kernel_size=KSIZE, padding=PAD, output_padding=OUTPAD, stride=2),
             nn.SiLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            # nn.SiLU(),
+            # nn.Conv2d(128, 128, kernel_size=3, padding=1),
             nn.SiLU(),
             nn.ConvTranspose2d(128, 64, kernel_size=KSIZE, padding=PAD, output_padding=OUTPAD, stride=2),
             nn.SiLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            # nn.SiLU(),
+            # nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.SiLU(),
             nn.ConvTranspose2d(64, 32, kernel_size=KSIZE, padding=PAD, output_padding=OUTPAD, stride=2),
             nn.SiLU(),
             nn.Conv2d(32, outchannels, kernel_size=3, padding=1),
             nn.SiLU(),
             nn.Conv2d(outchannels, outchannels, kernel_size=3, padding=1),
-            nn.Sigmoid(),
+            # nn.Sigmoid(),
         )
         
     def forward(self, z):
@@ -82,8 +94,9 @@ class ControlSignalDecoder(torch.nn.Module):
     
         
 class ControlSignalVAE(pl.LightningModule):
-    def __init__(self, pixelchannels=3, latentchannels=4):
+    def __init__(self, pixelchannels=3, latentchannels=4, lamda=0.1):
         super().__init__()
+        self.lamda = lamda
         self.encoder = ControlSignalEncoder(pixelchannels, latentchannels)
         self.decoder = ControlSignalDecoder(latentchannels, pixelchannels)
         
@@ -108,42 +121,163 @@ class ControlSignalVAE(pl.LightningModule):
         xhat = self.decode(z)
         return xhat, mu, logvar
     
+    def _forward(self, x):
+        mu, logvar = self.encoder(x)
+        xhat = self.decoder(mu)
+        return xhat, mu, logvar
+        
+    
     
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-4)
     
     def training_step(self, batch, batch_idx):
-        x = batch["cond_image"]
+        # x = batch["cond_image"]
+        x = batch.float()
 
-        xrecons, mu, logvar = self.forward(x)
-        reconloss = torch.nn.functional.mse_loss(xrecons, x, size_average=False)
-        kld = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())    
+        xrecons, mu, logvar = self._forward(x)
+        reconloss = torch.nn.functional.mse_loss(xrecons, x, reduction="none").mean()
+        # kld = -0.5 * torch.mean(1 + logvar - torch.pow(mu, 2) - torch.exp(logvar))    
+        # kld = kld * self.lamda
+        kld = self.lamda * torch.norm(mu, 2, dim=(1, 2, 3)).mean()
         loss = reconloss + kld    
+        
 
-        self.log_dict({
+        logdict = {
             'loss': loss.mean(),
             'kld': kld,
             'reconloss': reconloss.mean(),
-        })
+        }
+        return logdict
+        # self.log_dict(logdict)
 
-        return loss.mean()
+        # return loss.mean()
+        
+        
+class AccumulatedDict(object):
+    def __init__(self):
+        self.sums = {}
+        self.counts = {}
+        
+    def add(self, d):
+        for k, v in d.items():
+            if isinstance(v, torch.Tensor):
+                v = v.detach()
+                if v.dim() == 0:
+                    v = v.cpu().item()
+            if k not in self.sums:
+                self.sums[k] = 0
+            self.sums[k] += v
+            if k not in self.counts:
+                self.counts[k] = 0
+            self.counts[k] += 1
+            
+    def reset(self):
+        for k in self.sums:
+            self.sums[k] = 0
+        for k in self.counts:
+            self.counts[k] = 0
+            
+    def aggregate(self):
+        ret = {}
+        for k in self.sums:
+            ret[k] = self.sums[k] / self.counts[k]
+        return ret        
     
     
-def main():
-    model = ControlSignalVAE()
+def image_grid(imgs, rows, cols):
+    assert len(imgs) == rows*cols
+
+    w, h = imgs[0].size
+    grid = Image.new('RGB', size=(cols*w, rows*h))
+    grid_w, grid_h = grid.size
     
-    test_x = torch.randn(2, 3, 512, 512)
-    test_x_recon, *_ = model(test_x)
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i%cols*w, i//cols*h))
+    return grid
+
+
+def randomcolor_hsv():
+    hue = random.uniform(0, 360)
+    sat = random.uniform(0.4, 0.9)
+    val = random.uniform(0.2, 0.8)
+    return colorsys.hsv_to_rgb(hue/360, sat, val)
+
+
+def objectmasks_to_pil(x, colors):
+    colors = torch.tensor(colors).to(x.device)
+    argmax = torch.argmax(x, 0)
+    colormap = colors[argmax]
+    colormap = colormap.permute(2, 0, 1)
+    ret = transforms.ToPILImage()(colormap)
+    return ret
     
-    print(torch.nn.functional.mse_loss(test_x_recon, test_x))
     
+def main(epochs=100, gpu=0, outdir="control_ae_output", batsize=32, lamda=0.1, plotevery=200):
+    NUMOBJ = 20
+    model = ControlSignalVAE(pixelchannels=NUMOBJ+1, lamda=lamda)
     
-    cocodataset = COCOPanopticDataset(maindir="/USERSPACE/lukovdg1/coco2017", split="val", upscale_to=512)
+    randomcolors = [randomcolor_hsv() for _ in range(NUMOBJ+1)]
+    
+    device = torch.device("cuda", gpu)    
+    
+    cocodataset = COCOPanopticDataset(maindir="/USERSPACE/lukovdg1/coco2017", split="train", upscale_to=512, max_masks=NUMOBJ)
     print(len(cocodataset))
-    dl = DataLoader(cocodataset, batch_size=32, num_workers=10)
+    dl = DataLoader(cocodataset, batch_size=batsize, num_workers=10)
     
-    trainer = pl.Trainer(max_epochs=20, devices=1, accelerator="gpu")
-    trainer.fit(model, dl)
+    model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-4) 
+    
+    outpath = Path(outdir)
+    outpath.mkdir(parents=True, exist_ok=True)
+    (outpath / "images").mkdir(exist_ok=True)
+    
+    maximg = 10
+    global_step = 0
+    
+    totalsteps = epochs * len(dl)
+    print(f"Total steps: {totalsteps}")
+    
+    for epoch in range(epochs):
+        print(f"Epoch {epoch}")
+        accdict = AccumulatedDict()
+        for i, batch in tqdm.tqdm(enumerate(dl)):
+            optimizer.zero_grad()
+            x = batch["cond_image"].to(device)
+            
+            if global_step % plotevery == 0:
+                with torch.no_grad():
+                    showimgs = []
+                    showimgs_r = []
+                    x_ = x[:min(len(x), maximg)]
+                    xr = model._forward(x_)[0]
+                    for j in range(len(x_)):
+                        showimgs.append(objectmasks_to_pil(x_[j], randomcolors))
+                        showimgs_r.append(objectmasks_to_pil(xr[j], randomcolors))
+                    gridimage = image_grid(showimgs + showimgs_r, 2, len(showimgs))
+                    gridimage.save(outpath / "images" / f"reconstr_{global_step}.png")
+                
+            outdict = model.training_step(x, None)
+            accdict.add(outdict)
+            loss = outdict["loss"]
+            loss.backward()
+            optimizer.step()
+            global_step += 1
+            
+            
+                    
+        agg = accdict.aggregate()
+        
+            
+        torch.save(model.state_dict(), outpath / "ae.pth")
+        torch.save(model.encoder.state_dict(), outpath / "encoder.pth")
+        print(f"Epoch {epoch}: mse={agg['reconloss']}, kld={agg['kld']}")
+            
+            
+            
+    
+    # trainer = pl.Trainer(max_epochs=20, devices=1, accelerator="gpu")
+    # trainer.fit(model, dl)
     
     
     
